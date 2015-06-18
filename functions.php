4a6039f12ddb7f416c99571ef7f8baa9dc69f2fb
Chandra Prakash Thapa <cpthapa@gmail.com>
2015-06-18 15:51:35 +0545

    Complete design changed

--- functions.php
+++ functions.php
@@ -4,5 +4,10 @@
 require_once "lib/custom-data.php";
 
 
+	// This theme uses wp_nav_menu() in two locations.
+	register_nav_menus( array(
+		'primary' => __( 'Primary Menu',      'slc' ),
+
+	) );
 
 
--- index.php
+++ index.php
@@ -1,46 +1,188 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+<?php get_header(); ?>
 
-  <!-- Basic Page Needs
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta charset="utf-8">
-  <title>SLC Result 2071/2072 Nepal. Get School Leaving Certificate Result.</title>
-  <meta name="description" content="">
-  <meta name="author" content="">
 
-  <!-- Mobile Specific Metas
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+         <!-- Five -->
+        <section id="five" class="wrapper style2 special fade" style="height:700px!important;padding-top:12em;">
+          <div class="container">
+            <header>
+              <h2>School Leaving Certifiate (SLC) Result 2071 / 2072</h2>
+              <p>Enter your SLC Symbol No.</p>
+            </header>
+            <form method="post" action="#" class="container 50%">
+              <div class="row uniform 50%">
+                <div class="8u 12u$(xsmall)"><input type="text" name="symbol" id="symbol" placeholder="Your Symbol No." /></div>
+                <div class="4u$ 12u$(xsmall)"><input type="submit" value="Get Result" class="fit special" /></div>
+              </div>
+            </form>
+          </div>
+        </section>
 
-  <!-- FONT
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
 
-  <!-- CSS
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/normalize.css">
-  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/skeleton.css">
+      <!-- Banner 
+        <section id="banner">
+          <div class="content">
+            <header>
+              <h2>The future has landed</h2>
+              <p>And there are no hoverboards or flying cars.<br />
+              Just apps. Lots of mother flipping apps.</p>
+            </header>
+            <span class="image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pic01.jpg" alt="" /></span>
+          </div>
+          <a href="#one" class="goto-next scrolly">Next</a>
+        </section>
 
-  <!-- Favicon
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png">
+      <!-- One
+        <section id="one" class="spotlight style1 bottom">
+          <span class="image fit main"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pic02.jpg" alt="" /></span>
+          <div class="content">
+            <div class="container">
+              <div class="row">
+                <div class="4u 12u$(medium)">
+                  <header>
+                    <h2>Odio faucibus ipsum integer consequat</h2>
+                    <p>Nascetur eu nibh vestibulum amet gravida nascetur praesent</p>
+                  </header>
+                </div>
+                <div class="4u 12u$(medium)">
+                  <p>Feugiat accumsan lorem eu ac lorem amet sed accumsan donec.
+                  Blandit orci porttitor semper. Arcu phasellus tortor enim mi
+                  nisi praesent dolor adipiscing. Integer mi sed nascetur cep aliquet
+                  augue varius tempus lobortis porttitor accumsan consequat
+                  adipiscing lorem dolor.</p>
+                </div>
+                <div class="4u$ 12u$(medium)">
+                  <p>Morbi enim nascetur et placerat lorem sed iaculis neque ante
+                  adipiscing adipiscing metus massa. Blandit orci porttitor semper.
+                  Arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer
+                  mi sed nascetur cep aliquet augue varius tempus. Feugiat lorem
+                  ipsum dolor nullam.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <a href="#two" class="goto-next scrolly">Next</a>
+        </section>
 
-</head>
-<body>
+      <!-- Two 
+        <section id="two" class="spotlight style2 right">
+          <span class="image fit main"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pic03.jpg" alt="" /></span>
+          <div class="content">
+            <header>
+              <h2>Interdum amet non magna accumsan</h2>
+              <p>Nunc commodo accumsan eget id nisi eu col volutpat magna</p>
+            </header>
+            <p>Feugiat accumsan lorem eu ac lorem amet ac arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer mi sed nascetur cep aliquet augue varius tempus lobortis porttitor lorem et accumsan consequat adipiscing lorem.</p>
+            <ul class="actions">
+              <li><a href="#" class="button">Learn More</a></li>
+            </ul>
+          </div>
+          <a href="#three" class="goto-next scrolly">Next</a>
+        </section>
 
-  <!-- Primary Page Layout
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <div class="container">
-    <div class="row">
-      <div class="one-half column" style="margin-top: 25%">
-        <h4>Comming Soon!</h4>
-        <p>We will be right back!</a>.</p>
-      </div>
+      <!-- Three 
+        <section id="three" class="spotlight style3 left">
+          <span class="image fit main bottom"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pic04.jpg" alt="" /></span>
+          <div class="content">
+            <header>
+              <h2>Interdum felis blandit praesent sed augue</h2>
+              <p>Accumsan integer ultricies aliquam vel massa sapien phasellus</p>
+            </header>
+            <p>Feugiat accumsan lorem eu ac lorem amet ac arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer mi sed nascetur cep aliquet augue varius tempus lobortis porttitor lorem et accumsan consequat adipiscing lorem.</p>
+            <ul class="actions">
+              <li><a href="#" class="button">Learn More</a></li>
+            </ul>
+          </div>
+          <a href="#four" class="goto-next scrolly">Next</a>
+        </section>
+
+      <!-- Four 
+        <section id="four" class="wrapper style1 special fade-up">
+          <div class="container">
+            <header class="major">
+              <h2>Accumsan sed tempus adipiscing blandit</h2>
+              <p>Iaculis ac volutpat vis non enim gravida nisi faucibus posuere arcu consequat</p>
+            </header>
+            <div class="box alt">
+              <div class="row uniform">
+                <section class="4u 6u(medium) 12u$(xsmall)">
+                  <span class="icon alt major fa-area-chart"></span>
+                  <h3>Ipsum sed commodo</h3>
+                  <p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
+                </section>
+                <section class="4u 6u$(medium) 12u$(xsmall)">
+                  <span class="icon alt major fa-comment"></span>
+                  <h3>Eleifend lorem ornare</h3>
+                  <p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
+                </section>
+                <section class="4u$ 6u(medium) 12u$(xsmall)">
+                  <span class="icon alt major fa-flask"></span>
+                  <h3>Cubilia cep lobortis</h3>
+                  <p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
+                </section>
+                <section class="4u 6u$(medium) 12u$(xsmall)">
+                  <span class="icon alt major fa-paper-plane"></span>
+                  <h3>Non semper interdum</h3>
+                  <p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
+                </section>
+                <section class="4u 6u(medium) 12u$(xsmall)">
+                  <span class="icon alt major fa-file"></span>
+                  <h3>Odio laoreet accumsan</h3>
+                  <p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
+                </section>
+                <section class="4u$ 6u$(medium) 12u$(xsmall)">
+                  <span class="icon alt major fa-lock"></span>
+                  <h3>Massa arcu accumsan</h3>
+                  <p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
+                </section>
+              </div>
+            </div>
+            <footer class="major">
+              <ul class="actions">
+                <li><a href="#" class="button">Magna sed feugiat</a></li>
+              </ul>
+            </footer>
+          </div>
+        </section>
+
+     
+
+      <!-- Footer -->
+        <footer id="footer">
+          <ul class="icons">
+            <li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+            <li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+            <li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+
+          </ul>
+          <ul class="copyright">
+            <li>&copy; 2071-2071 SLC Result.All right Reserved.</li><li>Design: <a href="http://merobox.com">Mero Box</a></li>
+          </ul>
+        </footer>
+
     </div>
-  </div>
 
-<!-- End Document
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-</body>
+    <!-- Scripts -->
+      <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/jquery.min.js"></script>
+      <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/jquery.scrolly.min.js"></script>
+      <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/jquery.dropotron.min.js"></script>
+      <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/jquery.scrollex.min.js"></script>
+      <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/skel.min.js"></script>
+      <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/util.js"></script>
+      <!--[if lte IE 8]><script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/ie/respond.min.js"></script><![endif]-->
+      <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/main.js"></script>
+
+  </body>
 </html>
+
+        <?php 
+        /*
+        $table = new Table( 'tablename' );
+echo $table->insert( array('id' => 2, 'name' => 'Ram') );
+
+$table = new Table( 'tablename' );
+$all_rows = $table->get_all();
+
+echo print_r($all_rows);
+
+?>
+
